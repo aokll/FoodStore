4647f1a9e02742b17716ef6992a5b7c569e2bfb0
aokll <pastuhov_1992@mail.ru>
2024-05-25 13:51:37 +0300

    HW11

--- src/main/java/ru/gb/FoodStore/service/MenuService.java
+++ src/main/java/ru/gb/FoodStore/service/MenuService.java
@@ -1,22 +1,29 @@
 package ru.gb.FoodStore.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.gb.FoodStore.Aspects.TrackUserAction;
 import ru.gb.FoodStore.model.Menu;
 import ru.gb.FoodStore.repository.MenuRepository;
+import io.micrometer.core.instrument.Counter;
 
 import java.util.List;
 
 @Service
+
 public class MenuService {
     @Autowired
     private final MenuRepository menuRepository;
+//    @Autowired
+//    private final Counter myCounter;
 
-    public MenuService(MenuRepository userRepository) {
+    public MenuService(MenuRepository userRepository,MeterRegistry meterRegistry) {
         this.menuRepository = userRepository;
+//        this.myCounter = Counter.builder("my_custom_counter")
+//                .description("Counts something very important")
+//                .register(meterRegistry);
     }
-
 
     public List<Menu> findAll(){
         return menuRepository.findAll();
@@ -32,5 +39,7 @@ public class MenuService {
     }
     public void updateById(int id, String firstCourse, String secondCourse, String drink, int prise){
         menuRepository.updateById(id, firstCourse, secondCourse, drink, prise);
+        //myCounter.increment();//каждый раз когда вызывается метод updateById,
+        //счетчик увеличивается на 1.
     }
 }
